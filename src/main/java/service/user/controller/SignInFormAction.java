c9afc7b1faceba2d2e6be3b9d542f603520c96e6
it-improvement <0719improvement@naver.com>
2024-05-02 17:19:26 +0900

    Add signIn 로직 개발

--- src/main/java/service/user/controller/SignInFormAction.java
+++ src/main/java/service/user/controller/SignInFormAction.java
@@ -7,16 +7,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.user.model.UserDao;
+import service.user.model.UserResponseDto;
+
 public class SignInFormAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public SignInFormAction() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public SignInFormAction() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		System.out.println(id);
+		System.out.println(password);
+		
+		UserDao dao = UserDao.getInstance();
+		UserResponseDto dto = dao.findByIdAndPassword(id, password);
+		
+		if (dto == null) {
+			System.out.println("로그인 실패");
+			response.sendRedirect("/signIn");
+		} else {
+			response.sendRedirect("");
+		}
 	}
 
 }
--- src/main/java/service/user/model/UserDao.java
+++ src/main/java/service/user/model/UserDao.java
@@ -1,21 +1,23 @@
 package service.user.model;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import service.util.DBConnection;
 import service.util.PasswordCrypto;
 
 public class UserDao extends DBConnection {
-	
+
 	private UserDao() {
-		
+
 	}
-	
+
 	private static UserDao instance = new UserDao();
 
 	// 3. 단일 인스턴스에 대한 getter
 	public static UserDao getInstance() {
 		return instance;
 	}
-	
+
 	public UserResponseDto createUser(UserRequestDto dto) {
 		try {
 			String sql = "INSERT INTO user(id, password, name, phonenumber, email, birthday, gender) VALUES(?, ?, ?, ?, ?, ?, ?)";
@@ -31,14 +33,45 @@ public class UserDao extends DBConnection {
 			String gender = Integer.parseInt(dto.getGender()) % 2 == 1 ? "M" : "F";
 			System.out.println(gender);
 			pstmt.setString(7, gender);
-			
+
 			pstmt.execute();
-			
+
 			UserResponseDto userResponseDto = new UserResponseDto(dto);
 			return userResponseDto;
 		} catch (Exception e) {
 			e.printStackTrace();
+			System.out.println("회원가입 실패");
 		}
 		return null;
+	}
+
+	public UserResponseDto findByIdAndPassword(String targetId, String targetPassword) {
+		UserResponseDto dto = null;
+		try {
+			String sql = "select * from user where id=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, targetId);
+
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				System.out.println(rs.getString("password"));
+				if (!PasswordCrypto.decrypto(targetPassword, rs.getString("password"))) {
+					System.out.println("비밀번호 오류");
+					return null;
+				}
+				String id = rs.getString("id");
+				String password = rs.getString("password");
+				String name = rs.getString("name");
+				String phonenumber = rs.getString("phonenumber");
+				String email = rs.getString("email");
+				String birthday = rs.getString("birthday");
+				String gender = rs.getString("gender");
+				dto = new UserResponseDto(id, password, name, phonenumber, email, birthday, gender);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("로그인 로직 실패");
+		}
+		return dto;
 	}
 }
